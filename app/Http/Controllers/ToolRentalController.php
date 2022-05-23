8716c0e6955c2b215b9424cd03835388f56dddd2
arzyfinly8 <ragarigot3@gmail.com>
2022-05-24 02:36:07 +0700

    make table rental

---
+++ app/Http/Controllers/ToolRentalController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ToolRental;
+use Illuminate\Http\Request;
+
+class ToolRentalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ToolRental  $toolRental
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ToolRental $toolRental)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ToolRental  $toolRental
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ToolRental $toolRental)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ToolRental  $toolRental
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ToolRental $toolRental)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ToolRental  $toolRental
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ToolRental $toolRental)
+    {
+        //
+    }
+}
---
+++ app/Models/ToolRental.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ToolRental extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'inventaris_id',
+        'tenant',
+        'type',
+        'number_of_loan',
+        'rent_of_day',
+        'total_price',
+    ];
+    protected $with = ['inventaris_id'];
+
+    public function inventaris(){
+        $this->belongsTo(Inventaris::class, 'inventaris_id');
+    }
+}
